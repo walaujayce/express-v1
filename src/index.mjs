a46a738a623221e18932e1fe3351acb6f8da2c6d
Jayce <jaycejacquez9494@gmail.com>
2024-11-26 23:31:49 +0800

    no message

--- src/index.mjs
+++ src/index.mjs
@@ -16,6 +16,7 @@ const mockUsers = [
     { id:3, username: "roselina", displayName: "ROSELINA"}
 ];
 
+// GET API
 app.get('/',(request,response)=>{
     response.status(201).send({msg:"Hello"});
 });
@@ -45,10 +46,68 @@ app.get('/api/products', (request, response)=>{
     response.send([{id:123, name: "chicken breast", price: 12.99}]);
 });
 
+// POST API
+
 app.post('/api/users',(request,response)=>{
     console.log(request.body);
     const {body} = request;
     const newUsers = {id:mockUsers[mockUsers.length-1].id+1, ...body};
     mockUsers.push(newUsers);
     return response.status(201).send(newUsers);
+});
+
+// PUT API
+
+app.put('/api/users/:id', (request, response)=>{
+    const { body, params : {id}} = request;
+    const parseId = parseInt(id);
+    if(isNaN(parseId)) return response.sendStatus(400);
+
+    const findUsersIndex = mockUsers.findIndex(
+        (user) => user.id === parseId
+    );
+
+    if(findUsersIndex === -1) return response.sendStatus(404);
+
+    mockUsers[findUsersIndex] = { id: parseId, ...body };
+
+    return response.sendStatus(200);
+
+});
+
+// PATCH API
+
+app.patch('/api/users/:id', (request, response)=>{
+    const { body, params : {id}} = request;
+    const parseId = parseInt(id);
+    if(isNaN(parseId)) return response.sendStatus(400);
+
+    const findUsersIndex = mockUsers.findIndex(
+        (user) => user.id === parseId
+    );
+
+    if(findUsersIndex === -1) return response.sendStatus(404);
+
+    mockUsers[findUsersIndex] = { ...mockUsers[findUsersIndex], ...body };
+
+    return response.sendStatus(200);
+
+});
+
+// PATCH API
+
+app.delete('/api/users/:id',(request, response)=>{
+    const {params:{id}} = request;
+
+    const parseId = parseInt(id);
+
+    if(isNaN(parseId)) return response.sendStatus(400);
+
+    const findUsersIndex = mockUsers.findIndex((user)=> user.id === parseId);
+
+    if(findUsersIndex === -1) return response.sendStatus(404);
+
+    mockUsers.splice(findUsersIndex,1);
+
+    return response.sendStatus(200);
 });
